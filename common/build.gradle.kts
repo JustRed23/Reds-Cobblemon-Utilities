bdb964e655d19ff8e9baefc4d1d997451d86c5b3
JustRed23 <manuel.bormans@gmail.com>
2025-03-18 16:14:39 +0100

    Move common mixin file to correct location

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("com.cobblemon:mod:${property("cobblemon_version")}") { isTransitive = false }
 
+    //used for mixin classes
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
 }
